0898cc120f4985e0386f648e51c2c2280b2baa28
NRORAROADASSISTANCE <abhishek.maskuri@gmail.com>
2026-08-26 08:45:16 +0530

    Remove redundant employees wrapper; use consolidated API

--- frontend/api/employees.js
+++
@@ -1,8 +0,0 @@
-import handler from "./[...path].js";
-
-export default async function employeesHandler(req, res) {
-  const pathname = String(req.url || "").split("?")[0];
-  const path = pathname.replace(/^\/api\//, "").replace(/^\/+|\/+$/g, "");
-  req.query = { ...(req.query || {}), path };
-  return handler(req, res);
-}

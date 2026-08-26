c43702b005b4fc173bc9fbcdffff3c017a599ee3
NRORAROADASSISTANCE <abhishek.maskuri@gmail.com>
2026-08-26 08:45:09 +0530

    Remove redundant customer id wrapper; use consolidated customer API

--- frontend/api/customers/[id].js
+++
@@ -1,7 +0,0 @@
-import handler from "../[...path].js";
-
-export default async function customerByIdHandler(req, res) {
-  const id = req.query?.id;
-  req.query = { ...(req.query || {}), path: ["customers", String(id)] };
-  return handler(req, res);
-}
